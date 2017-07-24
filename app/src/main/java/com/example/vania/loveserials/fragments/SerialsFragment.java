ec2b0d8911ec43270781c7e74559c3888a8132ef
hrynchyshynHub <vaniahrynchyshyn@gmail.com>
2017-07-24 21:27:42 +0300

    commit

---
+++ app/src/main/java/com/example/vania/loveserials/fragments/SerialsFragment.java
@@ -0,0 +1,10 @@
+package com.example.vania.loveserials.fragments;
+
+import android.app.Fragment;
+
+/**
+ * Created by vania on 23.07.2017.
+ */
+public class SerialsFragment extends Fragment {
+
+}
